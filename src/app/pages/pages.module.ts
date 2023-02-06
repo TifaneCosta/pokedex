80c558ddd3f9f8bfa0ddb2292291bd5df2089906
TifaneCosta <costatifane@yahoo.com>
2023-02-06 10:01:39 -0300

    primeiro commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+
+//Module Routing
+import { RoutingModule } from './routing.module';
+
+//Moudule
+import { SharedModule } from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    SharedModule,
+  ]
+})
+export class PagesModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Components
+import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { PokeSearchComponent } from './poke-search/poke-search.component';
+
+
+
+@NgModule({
+  declarations: [
+    PokeHeaderComponent,
+    PokeSearchComponent
+  ],
+exports: [
+    PokeHeaderComponent,
+    PokeSearchComponent,
+],
+
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
